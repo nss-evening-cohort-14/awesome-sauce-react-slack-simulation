260a5ac07c3b979ffffd4604c7c6b8fa17c1cc84
Caseywalker <casey.walker118@gmail.com>
2021-05-10 20:56:13 -0500

    Initial setup

---
+++ src/components/NavBar.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Button
+} from 'reactstrap';
+import { signInUser, signOutUser } from '../helpers/data/auth';
+
+const NavBar = ({ user }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Slack Sauce</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <Link className="nav-link" to="/organizations">Organizations</Link>
+          </NavItem>
+          <NavItem>
+            <Link className="nav-link" to="/channels">Channels</Link>
+          </NavItem>
+          {
+            user !== null
+            && <NavItem>
+              {
+                user
+                  ? <Button color='danger' onClick={signOutUser}> Sign Out </Button>
+                  : <Button color='info' onClick={signInUser}> Sign In </Button>
+              }
+            </NavItem>
+          }
+        </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+NavBar.propTypes = {
+  user: PropTypes.any
+};
+
+export default NavBar;
