1ba4db62267ce993b30ab79ba4febfbfe986b4c1
fimoefive <brothermj@hotmail.com>
2021-05-12 17:11:38 -0500

    imported useState and Button in Organization.js file. Also added an handleClick function to setAddOrganization and declared a setAddOrganization set to false within the Organizations function. Also added a user PropType at the bottom of the file.

--- src/views/AddOrganization.js
+++ src/views/AddOrganization.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import OrganizationForm from '../components/OrganizationsForm';
+// import OrganizationForm from '../components/OrganizationsForm';
 
 function AddOrganization({ setOrganizations }) {
   return (
     <div>
-      <OrganizationForm
+      {/* <OrganizationForm */}
         formTitle='Add Organization'
         setOrganizations={setOrganizations}
       />
--- src/views/Organizations.js
+++ src/views/Organizations.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
 import OrganizationCard from '../components/OrganizationCard';
+import OrganizationForm from '../components/OrganizationsForm';
 
-function Organizations({ organizations, setOrganizations }) {
+function Organizations({ user, organizations, setOrganizations }) {
+  const [showAddOrganization, setAddOrganization] = useState(false);
+
+  const handleClick = () => {
+    setAddOrganization((prevState) => !prevState);
+  };
+
   return (
     <>
       <div className="card-container" >
+        <div>
+          {!showAddOrganization
+            ? <Button onClick={handleClick}>Add Organization</Button>
+            : <div>
+              <Button onClick={handleClick}>Close</Button>
+              <OrganizationForm
+                setOrganizations={setOrganizations}
+                user={user}
+              />
+            </div>
+          }
+        </div>
         {organizations.map((orgInfo) => (
           <OrganizationCard
             key={orgInfo.firebaseKey}
@@ -13,6 +33,7 @@ function Organizations({ organizations, setOrganizations }) {
             icon={orgInfo.icon}
             organizationName={orgInfo.organizationName}
             setOrganizations={setOrganizations}
+            user={user}
           />
         ))};
     </div>
@@ -22,7 +43,8 @@ function Organizations({ organizations, setOrganizations }) {
 
 Organizations.propTypes = {
   organizations: PropTypes.array.isRequired,
-  setOrganizations: PropTypes.func.isRequired
+  setOrganizations: PropTypes.func.isRequired,
+  user: PropTypes.any
 };
 
 export default Organizations;
