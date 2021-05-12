3ac32021fc936e53e3ed4f16f24dee3aa3f2e3b6
fimoefive <brothermj@hotmail.com>
2021-05-11 19:08:05 -0500

    added updateOrganization function in the organizationData.js file. Also added OrganizationForm function with a handleInputChange, and handleSubmit functions in the OrganizationFrom.js file.

---
+++ src/components/OrganizationsForm.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button, Form, FormGroup, Label, Input
+} from 'reactstrap';
+import addOrganization from '../helpers/data/organizationData';
+
+const OrganizationForm = ({
+  formTitle,
+  setOrganizations,
+  firebaseKey,
+  icon,
+  organizationName
+}) => {
+  const [organization, setOrganization] = useState({
+    firebaseKey: firebaseKey || null,
+    icon: icon || '',
+    organizationName: organizationName || '',
+  });
+
+  const handleInputChange = (e) => {
+    setOrganization((prevState) => ({
+      ...prevState,
+      [e.target.name]:
+        e.target.name === 'position' ? (e.target.value) : e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (organization.firebaseKey) {
+      updateOrganization(org).then((orgArray) => setOrganization(orgArray));
+    } else {
+      addOrganization(org).then((response) => {
+        setOrganizations(response);
+      });
+
+      setOrganizations({
+        firebaseKey: null,
+        icon: '',
+        organizationName: ''
+      });
+    }
+  };
+
+  return (
+    <>
+      <div className='organization-form'>
+        <Form
+          id='addOrganizationForm'
+          autoComplete='off'
+          onSubmit={handleSubmit}
+        >
+          <h2>{formTitle}</h2>
+          <FormGroup>
+            <Label for="organizationName">Name: </Label>
+            <Input
+              organizationName='organizationName'
+              id='organizationName'
+              value={organization.organizationName}
+              type='text'
+              placeholder='Enter a Name'
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="icon">ICON: </Label>
+            <Input
+              name='icon'
+              id='icon'
+              value={organization.icon}
+              type='text'
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+OrganizationForm.propTypes = {
+  formTitle: PropTypes.string.isRequired,
+  setOrganizations: PropTypes.func,
+  firebaseKey: PropTypes.string,
+  icon: PropTypes.string,
+  organizationName: PropTypes.string
+};
+
+export default OrganizationForm;
