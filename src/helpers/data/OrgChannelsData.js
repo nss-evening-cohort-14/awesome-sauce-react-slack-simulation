410c0cba2ebbcdc2859e1e3072527a64d093ac61
fimoefive <brothermj@hotmail.com>
2021-05-18 18:29:26 -0500

    formatting changes in OrgChannelsData.js file.

--- src/helpers/data/OrgChannelsData.js
+++ src/helpers/data/OrgChannelsData.js
@@ -35,12 +35,6 @@ const deleteOrgChannels = (firebaseKey, user) => new Promise((resolve, reject) =
   }).catch((error) => reject(error));
 });
 
-// const deleteBoardPins = (firebaseKey, user) => new Promise((resolve, reject) => {
-//   getBoardPins(firebaseKey).then((pinBoardArray) => {
-//     const deletePins = pinBoardArray.map((pin) => deletePin(pin.firebaseKey, user));
-//     Promise.all(deletePins).then(() => resolve(deleteBoard(firebaseKey, user)));
-//   }).catch((error) => reject(error));
-// });
 export {
   getOrgChannelsJoin, getOrganizationChannels,
   deleteOrgChannels
